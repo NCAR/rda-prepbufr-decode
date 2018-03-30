e9a3f98fc60caf2685d5aa96618360362498deff
tcram <tcram@ucar.edu>
2018-03-30 17:02:57 -0600

    Debugging

--- src/readpb_config.f
+++ src/readpb_config.f
@@ -227,8 +227,8 @@ c     + at level/stack index = ",lv,jj
                   END DO
                       
                   if (count .lt. 64) then
-                    print*,subset(1:6),"var, lev, AN: ",
-     +lv,var(kk),evns(7,lv,jj,kk)
+                    print*,subset(1:6)," var, lev, AN: ",
+     +lv,var(kk),evns(6,lv,jj,kk)
                     WRITE  ( UNIT = 12, FMT = '(A200)' )  outstg
                   endif
                 END DO  ! End jj = 1, MXR8VN loop
@@ -300,8 +300,8 @@ c     + at level/stack index = ",lv,jj
                   END DO
                       
                   if (count .lt. 64) then
-                    print*,subset(1:6),"var, lev, AN: ",
-     +lv,var(kk),evns(7,lv,jj,kk)
+                    print*,subset(1:6)," var, lev, AN: ",
+     +lv,var(kk),evns(6,lv,jj,kk)
                     WRITE  ( UNIT = 12, FMT = '(A200)' )  outstg
                   endif
                 END DO  ! End jj = 1, MXR8VN loop
@@ -367,8 +367,8 @@ c     + at level/stack index = ",lv,jj
                   END DO
                       
                   if (count .lt. 64) then
-                    print*,subset(1:6),"var, lev, AN: ",
-     +lv,var(kk),evns(7,lv,jj,kk)
+                    print*,subset(1:6)," var, lev, AN: ",
+     +lv,var(kk),evns(6,lv,jj,kk)
                     WRITE  ( UNIT = 12, FMT = '(A200)' )  outstg
                   endif
 
