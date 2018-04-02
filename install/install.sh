0896d789a590e066aa120c45e8f0fc57a635e966
tcram <tcram@ucar.edu>
2018-04-02 11:42:44 -0600

    Updated install script

--- install/install.sh
+++ install/install.sh
@@ -10,7 +10,7 @@ SRC=../src
 EXE=../exe
 
 # Path to BUFRLIB.  Library available at 
-http://www.nco.ncep.noaa.gov/sib/decoders/BUFRLIB/
+#      http://www.nco.ncep.noaa.gov/sib/decoders/BUFRLIB/
 LIB=/glade/apps/opt/BUFRLIB/11.0.0/intel/12.1.5/lib  # path to BUFRLIB
 
 # Path to fortran compiler
